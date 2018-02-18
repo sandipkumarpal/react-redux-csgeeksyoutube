666bd1a6f95aa3815aeca369361831b3bf9b2283
Sandip Pal <sandip.pal18@gmail.com>
2018-02-18 21:09:53 +0530

    Added Book reducers under the reducer folder

--- redux/src/reducers/index.js
+++ redux/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import BooksReducers from './reducers_books';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducers
 });
 
 export default rootReducer;
---
+++ redux/src/reducers/reducers_books.js
@@ -0,0 +1,11 @@
+default function() {
+    return [
+        {title: 'Javascript'},
+        {title: 'Java'},
+        {title: 'Action Script'},
+        {title: 'Html'},
+        {title: 'Css'},
+        {title: 'Jquery'},
+        {title: 'Ruby Rails'}
+    ]
+}
